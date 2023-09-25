a489b5688528402453eee62bb654380b1578a22d
abelbirhanu1057@gmail.com <113086309+abel-b-degu@users.noreply.github.com>
2023-09-25 12:04:35 -0500

    Started working on navbar

---
+++ src/Pages/Home/Navbar.jsx
@@ -0,0 +1,43 @@
+import{useState, useEffect} from "react";
+import{Link} from "react-scroll";
+
+function Navbar(){
+    const[navActive, setNavActive]  = useState
+    (false);
+
+    const toggeNav = () =>{
+        setNavActive(!navActive)
+    }
+
+    const closeMenu = () =>{
+        setNavActive(false)
+    }
+
+    useEffect(() =>{
+        const handleResize = () =>{
+            if(window.innerWidth <= 500){
+                closeMenu
+        }
+    }
+    window.addEventListener("resize",
+     handleResize);
+
+
+     return() =>{
+        window.removeEventListener('resize',handleResize);
+     }
+    },[]);
+
+    useEffect(() =>{
+        if(window.innerWidth <= 1200){
+            closeMenu;
+        }
+    },{});
+
+    return(
+        <nav className={`navbar ${navActive? "active" : "" }`}>
+
+
+        </nav>
+    )
+}
